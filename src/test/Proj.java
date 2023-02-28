efaa18c1fe7e5ab7dcf59e5a5bf409d8e09015ef
ssjanani <ssjanani245@gmail.com>
2023-02-28 22:12:06 +0530

    Commit to new repo

---
+++ src/test/Proj.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Proj {
+	public static void main (String[] args)
+	{
+		int a=10;
+		int b=2;
+		int c;
+		c=a/b;
+		System.out.println("The value of C is:"+c);
+	}
+}
