9282345535a074d2c410bb26fbdea0c8f19c7d1d
ssjanani <ssjanani245@gmail.com>
2023-03-01 15:17:36 +0530

    Commit

--- src/test/Proj.java
+++ src/test/Proj.java
@@ -3,10 +3,10 @@ package test;
 public class Proj {
 	public static void main (String[] args)
 	{
-		int a=20;
+		int d=50;
 		int b=2;
 		int c;
-		c=a/b;
+		c=d/b;
 		System.out.println("The value of C is:"+c);
 	}
 }
